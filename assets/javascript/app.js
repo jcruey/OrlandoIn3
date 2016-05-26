405ad80c1fcd0f14b4f775b79bd1d20535c4aab8
sheli16 <sheli16@live.seminolestate.edu>
2016-05-25 20:39:00 -0400

    custom maps js and media queries css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -304,7 +304,21 @@ $(document).ready(function () {
                     firebaseEventSelect
                 });
                     
-                    
+                infobox = new InfoBox({
+    content: document.getElementById("infobox1"),
+    disableAutoPan: false,
+    maxWidth: 150,
+    pixelOffset: new google.maps.Size(-140, 0),
+    zIndex: null,
+    boxStyle: {
+                background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/examples/tipbox.gif') no-repeat",
+                opacity: 0.75,
+                width: "280px"
+        },
+    closeBoxMargin: "12px 4px 2px 2px",
+    closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif",
+    infoBoxClearance: new google.maps.Size(1, 1)
+});s    
 
             }); 
             return false;
